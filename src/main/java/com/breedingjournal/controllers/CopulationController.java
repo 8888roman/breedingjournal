c59a822a35a5f08aeba5ccf4f3ffa7dfdf8280a9
pawel <pawelrogozinski88@gmail.com>
2018-03-07 00:10:13 +0100

    thymeleaf security dialect working

--- src/main/java/com/breedingjournal/controllers/CopulationController.java
+++ src/main/java/com/breedingjournal/controllers/CopulationController.java
@@ -5,12 +5,14 @@ import com.breedingjournal.domain.Copulation;
 import com.breedingjournal.form.CopulationForm;
 import com.breedingjournal.repositories.CopulationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -70,6 +72,15 @@ public class CopulationController {
         return "copulationlist";
 
     }
+
+    @RequestMapping(value = "/copulationlist/{id}", method = RequestMethod.DELETE)
+    public String deleteCopulation(@PathVariable("id") Long id) {
+       copulationRepository.delete(id);
+        return "copulationlist";
+
+    }
+
+
 
     @RequestMapping(value="/logout", method = RequestMethod.GET)
     public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
